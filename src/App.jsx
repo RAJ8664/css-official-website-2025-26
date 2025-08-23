f8b7d17e7cd3d290c7c672adf8b93b7b64f3ceca
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2025-08-23 22:25:59 +0530

    Project setup #01

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='text-xl text-center'>
+      Computer Science Society NITS
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/NavbarMobile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavbarMobile = () => {
+  return (
+    <div>
+      NavbarMobile
+      
+      {/* Make the navbar mobile here */}
+    </div>
+  )
+}
+
+export default NavbarMobile
