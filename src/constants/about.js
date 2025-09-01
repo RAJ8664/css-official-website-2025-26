15edae583d3c8d50c559a1896525a54c5b0bb53b
OliviaNath <nathrina1880@gmail.com>
2025-09-01 22:09:37 +0530

    Add files via upload

---
+++ src/constants/about.js
@@ -0,0 +1,11 @@
+
+const page= {
+    "title": "About Us",
+    "slug": "about-us",
+    "content": {
+      "heading": "ABOUT US",
+      "body": "The Computer Science Society, run by the CSE department of NIT Silchar, aims to impart academic, technical and socio-cultural awareness to the students of our beloved college."
+    }
+}
+export default page;
+
